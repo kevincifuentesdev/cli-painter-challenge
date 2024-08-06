381ee784fc65c5037c661273d26a06a1ad644b44
Kevin Cifuentes <kevins_cifuentes@soy.sena.edu.co>
2024-08-05 22:05:17 -0500

    creacion de la clase "Circle" y sus metodos

--- painter/model.py
+++ painter/model.py
@@ -5,3 +5,21 @@ class Point:
     def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
+
+class Circle:
+    def __init__(self, center: Point, radius: float) -> None:
+        self.center = center
+        self.radius = radius
+
+    def area(self) -> float:
+        return math.pi * self.radius ** 2
+    
+    def draw(self) -> None:
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self) -> str:
+        return f"Circle with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
+    
